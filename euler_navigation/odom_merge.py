e0ad42048808f8de0bf205a4d5528dbed9f61be3
David V. Lu!! <davidvlu@gmail.com>
2014-12-08 18:12:22 -0600

    Odom Merge

---
+++ euler_navigation/odom_merge.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+
+class OdomMerger:
+    def __init__(self):
+        rospy.init_node('odom_merger')
+        self.sub1 = rospy.Subscriber('/euler/odom', Odometry, self.odom_cb)
+        self.sub2 = rospy.Subscriber('/euler/cmd_vel', Twist, self.twist_cb)
+        self.pub = rospy.Publisher('/euler/odom_smooth', Odometry)
+        self.twist = Twist()
+        
+    def twist_cb(self, msg):
+        self.twist = msg
+        
+    def odom_cb(self, msg):
+        msg.twist.twist = self.twist
+        self.pub.publish(msg)
+        
+if __name__=='__main__':
+    om = OdomMerger()
+    rospy.spin()
+        
